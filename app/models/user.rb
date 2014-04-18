093d3dbe40af6f14a5e6f7aa2d709158a80e1ee9
Moises Magno Moreira <moises@Moisess-iMac.local>
2014-04-17 21:04:47 -0300

    Extend chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 						uniqueness: {case_sensitive: false}
 
-	before_save {self.email = email.downcase}			
 	has_secure_password			
 	validates :password, length: {minimum: 6}
+
+	before_save {self.email = email.downcase}			
 end
